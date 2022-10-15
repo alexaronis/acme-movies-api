b4a9ab8f86775bcb9314d8e6efa8c51d033fe0d6
alexaronis <aronis.alex@gmail.com>
2022-10-15 15:26:08 +0000

    added GET

--- test/spec.js
+++ test/spec.js
@@ -1,11 +1,20 @@
 const { expect } = require("chai");
-
+const app = require("supertest")(require('../app'));
 
 
 describe('First test', ()=>{
     it('xyz equals 5', async()=>{
-        const xyz = 5
+        const xyz = 5;
         expect(xyz).to.equal(5);
-    })
+    });
+    
+})
+
+describe('Test GET', ()=>{
+    it('GET is working', async()=>{
+        const response = await app.get('/');
+        expect(response.status).to.equal(200);
+        expect(response.text).to.contain('hello');
+    });
     
 })
